da544774b85d5345c4f02b1cb8c08104059b042e
nada <nada.mesbah@usmba.ac.ma>
2023-02-02 13:17:08 +0100

    Add project files.

---
+++ Hubs/TextEditorHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CollaborationApp.Hubs
+{
+    public class TextEditorHub : Hub
+    {
+        public async Task UpdateDocument(string content)
+        {
+            await Clients.All.SendAsync("ReceiveUpdate", content);
+        }
+    }
+}
---
+++ wwwroot/js/textEditor.js
@@ -0,0 +1,15 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/hub/texteditor").build();
+connection.start().then(function () {
+    document.getElementById("updateButton").addEventListener("click", function (event) {
+        var content = document.getElementById("textEditor").value;
+        connection.invoke("UpdateDocument", content).catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    });
+    connection.on("ReceiveUpdate", function (content) {
+        document.getElementById("textEditor").value = content;
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
